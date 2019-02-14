7b45f5e2063f08e76669b0639c9487e4b7ac4b24
Anand Swaroop <Anand.swaroop@ATMECS.CORP>
2019-02-15 03:49:22 +0530

    View complete POST request

--- webdev/main.py
+++ webdev/main.py
@@ -3,9 +3,9 @@
 
 import webapp2
 
-# Adding another handler named "/testform" so that we can pass the query parameter to "/testform"
+# Adding attribute "method" in the form to "post". By default, this method is get and this was the reason we were not required to explicitly set it to get.
 form = """
-<form action="/testform">
+<form method="post" action="/testform"> 
 <input name="q">
 <input type="submit">
 </form>
@@ -16,18 +16,26 @@ class MainPage(webapp2.RequestHandler):
     	self.response.out.write(form) # Printing the form this time instead of "Hello World"
 
 class TestHandler(webapp2.RequestHandler): #New Handler
-    def get(self):
-    	self.response.headers['Content-Type'] = 'text/plain' # So that browser doesn't render the server response
-        self.response.out.write(self.request) # This time, we want to print the complete GET request which will show us ==> GET /testform?q=whatever as the first line 
+    def post(self): # Since we have changed the method in line #8 to post, we have to make the corresponding change to handler as well to avoid "method not allowed" errror. HTTP 405
+    	self.response.headers['Content-Type'] = 'text/plain'    # Since we have changed the method to post, we want to see complete request. Our query parameter 'q' will come after
+        self.response.out.write(self.request)                   # the HTTP request headers
 
 app = webapp2.WSGIApplication([('/', MainPage),('/testform', TestHandler)], debug=True)
 
-# Output of the above program is below:
-# GET /testform?q=whatever HTTP/1.1
+# Sample Output:
+# POST /testform HTTP/1.1
 # Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
 # Accept-Language: en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,hi;q=0.6
+# Cache-Control: max-age=0
+# Content-Length: 13
+# Content-Type: application/x-www-form-urlencoded
+# Content_Length: 13
+# Content_Type: application/x-www-form-urlencoded
 # Host: localhost:10080
+# Origin: http://localhost:10080
 # Referer: http://localhost:10080/
 # Upgrade-Insecure-Requests: 1
 # User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36
 # X-Appengine-Country: ZZ
+
+# q=who+are+you
