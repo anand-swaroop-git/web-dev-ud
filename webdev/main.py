086240b4823b60062b0a639cafdd24f80b20d355
Anand Swaroop <Anand.swaroop@ATMECS.CORP>
2019-02-15 02:53:57 +0530

    Adding the search engine logic

--- webdev/main.py
+++ webdev/main.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# https://webapp2.readthedocs.io/en/latest/
+
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+# Adding the simple search engine logic with the help of a form
+form = """
+<form action="http://www.google.com/search">
+<input name="q">
+<input type="submit">
+</form>
+"""
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+    	self.response.headers['Content-Type'] = 'text/html' # So that the browser can render the HTML
+        self.response.write(form) # Printing the form this time instead of "Hello World"
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
